b5d126550c51ddac63437994895cc6d183ef235e
areus-fundaciobit <areus@fundaciobit.org>
2019-07-04 09:10:21 +0200

    primer commit

---
+++ blueprint-ejb/src/main/java/org/fundaciobit/blueprint/ejb/jpa/Item.java
@@ -0,0 +1,56 @@
+package org.fundaciobit.blueprint.ejb.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Requisits de les classes persistents:
+ *   L'etiqueta Entity, constructor buid. Ni la classe ni els seus mètodes
+ * o camps poden ser final. Si les hem de passar com a paràmetres detached ha d'implementar serializable
+ * (i per tant també és recomanable el serialVersionUID).
+ * Els camps persistents han de ser private protected o package-private i només es poden accedir mitjançant els
+ * mètodes
+ */
+@Entity
+@SequenceGenerator(name="item-sequence", sequenceName = "BLP_ITEM_SEQ", allocationSize = 1)
+@Table(name = "BLP_ITEM", indexes =
+   @Index(name = "BLP_ITEM_PK_I", columnList = "ITEMID", unique = true)
+)
+public class Item implements Serializable {
+
+   private static final long serialVersionUID = 1L;
+
+   public Item() {
+   }
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "item-sequence")
+   @Column(name="ITEMID",nullable = false,length = 19)
+   private Long id;
+
+   @Column(name = "NAME", nullable = false, length = 1000)
+   private String name;
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+}
---
+++ blueprint-web/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: areus
+  Date: 3/6/2019
+  Time: 14:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello world!</title>
+</head>
+<body>
+<h1>Hello world!</h1>
+</body>
+</html>
