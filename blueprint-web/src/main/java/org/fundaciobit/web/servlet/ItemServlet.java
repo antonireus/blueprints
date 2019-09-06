78d54579edb20af39b4f7f77ec8ea0fb71da7693
areus-fundaciobit <areus@fundaciobit.org>
2019-09-06 22:18:16 +0200

    revisió warnings eclipse

--- blueprint-web/src/main/java/org/fundaciobit/web/servlet/ItemServlet.java
+++ blueprint-web/src/main/java/org/fundaciobit/web/servlet/ItemServlet.java
@@ -20,49 +20,49 @@ import java.util.logging.Logger;
 @WebServlet(name = "itemServlet", urlPatterns = "/item")
 public class ItemServlet extends HttpServlet {
 
-	 private static final long serialVersionUID = -7525166929518102623L;
+	private static final long serialVersionUID = -7525166929518102623L;
 
-	 @Inject
-    private Logger logger;
+	@Inject
+	private Logger logger;
 
 	@Resource
-    private Validator validator;
+	private Validator validator;
 
-    @EJB
-    private ItemService itemService;
+	@EJB
+	private ItemService itemService;
 
-    @Override
-    public void init() {
-        logger.info("init");
-    }
+	@Override
+	public void init() {
+		logger.info("init");
+	}
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-        logger.info("doGet");
-        request.setAttribute("items", itemService.findAll());
+		logger.info("doGet");
+		request.setAttribute("items", itemService.findAll());
 
-        request.getRequestDispatcher("/item/index.jsp").forward(request, response);
-    }
+		request.getRequestDispatcher("/item/index.jsp").forward(request, response);
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        logger.info("doPost");
+		logger.info("doPost");
 
-        Item item = new Item();
-        item.setName(request.getParameter("name"));
-        item.setNif(request.getParameter("nif"));
-        logger.info("itemService.create: " + item.getName() + ", " + item.getNif());
+		Item item = new Item();
+		item.setName(request.getParameter("name"));
+		item.setNif(request.getParameter("nif"));
+		logger.info("itemService.create: " + item.getName() + ", " + item.getNif());
 
-        Set<ConstraintViolation<Item>> constraintViolations = validator.validate(item);
-        if (constraintViolations.isEmpty()) {
-            itemService.create(item);
-        } else {
-            request.getSession().setAttribute("item_constraintViolations", constraintViolations);
-        }
+		Set<ConstraintViolation<Item>> constraintViolations = validator.validate(item);
+		if (constraintViolations.isEmpty()) {
+			itemService.create(item);
+		} else {
+			request.getSession().setAttribute("item_constraintViolations", constraintViolations);
+		}
 
-        response.sendRedirect(request.getContextPath() + "/item");
-    }
+		response.sendRedirect(request.getContextPath() + "/item");
+	}
 }
