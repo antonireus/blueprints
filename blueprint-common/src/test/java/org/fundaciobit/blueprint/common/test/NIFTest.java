3f4fdfeeab598f5d008e0e159993edcb1deaf3cd
areus-fundaciobit <areus@fundaciobit.org>
2019-09-17 22:37:45 +0200

    test de validaor de NIF

---
+++ blueprint-common/src/test/java/org/fundaciobit/blueprint/common/test/NIFTest.java
@@ -0,0 +1,34 @@
+package org.fundaciobit.blueprint.common.test;
+
+import org.fundaciobit.blueprint.common.constraint.NIF;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class NIFTest {
+
+    @NIF
+    private String nif;
+
+    @Test
+    public void testValidNIF() {
+        this.nif = "00000000T";
+        final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        final Set<ConstraintViolation<NIFTest>> validate = validatorFactory.getValidator().validate(this);
+        Assert.assertTrue(validate.isEmpty());
+    }
+
+    @Test
+    public void testInvalidNIF() {
+        this.nif = "00000000R";
+        final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        final Set<ConstraintViolation<NIFTest>> validate = validatorFactory.getValidator().validate(this);
+        Assert.assertFalse(validate.isEmpty());
+    }
+
+
+}
