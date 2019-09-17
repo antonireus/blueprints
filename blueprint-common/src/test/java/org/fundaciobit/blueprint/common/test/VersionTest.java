71e5ad116d480f0559d0082244bffd973c4b14f2
areus-fundaciobit <areus@fundaciobit.org>
2019-09-17 15:00:18 +0200

    petita revisió poms i incorporar test arquillian

---
+++ blueprint-common/src/test/java/org/fundaciobit/blueprint/common/test/VersionTest.java
@@ -0,0 +1,34 @@
+package org.fundaciobit.blueprint.common.test;
+
+import org.fundaciobit.blueprint.common.Version;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+@RunWith(Arquillian.class)
+public class VersionTest {
+
+    @Inject
+    Version version;
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+                .addClass(Version.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        System.out.println(jar.toString(true));
+        return jar;
+    }
+
+    @Test
+    public void testVersionNumber() {
+        Assert.assertEquals("0.9", version.getNumber());
+    }
+}
