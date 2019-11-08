c46cce7daaab07b01491bdf2ccee0c42f2ce58f8
areus-fundaciobit <areus@fundaciobit.org>
2019-11-08 14:20:53 +0100

    els paths no haurien de començar amb /

--- blueprint-rest/src/main/java/org/fundaciobit/blueprint/rest/resource/ItemResource.java
+++ blueprint-rest/src/main/java/org/fundaciobit/blueprint/rest/resource/ItemResource.java
@@ -6,19 +6,13 @@ import org.fundaciobit.blueprint.ejb.service.ItemService;
 
 import javax.ejb.EJB;
 import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.List;
 
-@Path("/item")
+@Path("item")
 public class ItemResource {
 
     @EJB
